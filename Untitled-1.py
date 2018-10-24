a866bf1bc74f846b1cb7a0aef496ee87bea1f22f
shiloh0810 <shiloh20040810@gmail.com>
2018-10-23 23:20:31 -0400

    changed a long time ago ;) idk what this actually is

---
+++ Untitled-1.py
@@ -0,0 +1,15 @@
+#encoding:utf-8
+
+# 範例二
+# python 和 JavaScript 很相像都有兩種版本的迴圈
+# range 是什麼可以看看這篇 http://www.runoob.com/python/python-func-range.html
+
+# while 迴圈版本
+x = 0
+while x < 10:
+    print str(x) + '隻羊'
+    x += 1
+
+# for 迴圈版本
+for y in range(10):
+    print str(y) + '朵花'
--- hello.py
+++ hello.py
@@ -1,28 +1,29 @@
 from random import randint
 number = randint(1000, 9999)
-# print number
+print number
 guess = str(raw_input("What's your guess?"))
 number_list = str(number)
+while guess != number_list:
+    b = 0
+    for i in guess:
+        if i == number_list[0]:
+            b += 1
+        if i == number_list[1]:
+            b += 1
+        if i == number_list[2]:
+            b += 1
+        if i == number_list[3]:
+            b += 1
 
-b = 0
-for i in guess:
-    if i == number_list[0]:
-        b += 1
-    if i == number_list[1]:
-        b += 1
-    if i == number_list[2]:
-        b += 1
-    if i == number_list[3]:
-        b += 1
+    a = 0
+    i = 0
+    while i < len(guess):
+        if guess[i] == number_list[i]:
+            a += 1
+        i += 1
 
-a = 0
-i = 0
-while i < len(guess):
-    if guess[i] == number_list[i]:
-        a += 1
-    i += 1
+    print str(a) + 'A' + str(b) + 'B'
+    guess = str(raw_input("What's your guess?"))
 
-print str(a) + 'A' + str(b) + 'B'
 
-if b == 4:
-    print "Congrats!"
+print "Congrats!"
